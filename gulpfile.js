a3ddaf2ef439fa1d47b43a8c41abafa86d778809
kseveru <kseveru@yandex.ru>
2018-08-06 15:34:13 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var htmlmin = require('gulp-htmlmin');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssmin = require('gulp-csso');
+var pump = require('pump');
+var browserSync = require("browser-sync").create();
+
+gulp.task('clean', function () {
+  return del('docs');
+});
+
+gulp.task('html', function() {
+  return gulp.src('assets/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('docs'));
+});
+
+gulp.task('style', function () {
+  return gulp.src('assets/style.css')
+    .pipe(postcss(autoprefixer({grid: true})))
+    .pipe(cssmin())
+    .pipe(gulp.dest('docs'));
+});
+
+gulp.task('dev', function() {
+  browserSync.init({
+    server: 'docs'
+  });
+  gulp.watch('assets/**/*.css', gulp.series('style'));
+  gulp.watch('assets/*.html', gulp.series('html'));
+  browserSync.watch('./**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('build',
+  gulp.series('clean',
+  gulp.parallel('html', 'style')
+  )
+);
+
+gulp.task('default',
+  gulp.series('clean',
+  gulp.parallel('html', 'style'),
+  'dev')
+);
